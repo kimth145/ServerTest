d66486c3d42c5ce7b12591df63b918e13b372e9c
kimth <kimth145@ais-info.co.jp>
2022-07-14 11:48:15 +0900

    3 commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.send('Hello World!!!!~~')
 })
 
+app.get('/g/', (req, res)=>{
+    res.send('Hello Kim!!!!~~')
+})
+
 app.listen(process.env.PORT || 5000, ()=>{
     console.log('Server Started!!')
 })
