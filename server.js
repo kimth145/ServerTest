6a9c83bf75d49057734a4db3b95ecb04b71ecdd3
kimth <kimth145@ais-info.co.jp>
2022-07-14 13:58:36 +0900

    5 commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', (req, res)=>{
+    console.log('loot')
     res.send('Hello World!!!!~~')
 })
 
 app.get('/g/', (req, res)=>{
+    console.log('g')
     res.send('Hello Kim!!!!~~')
 })
 
